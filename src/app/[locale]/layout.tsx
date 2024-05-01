4a4e5d13a30234218d7748225255af751c1c0ad5
Tejjas Kaul <tejjaskaul@Tejjass-MacBook-Pro.local>
2024-05-01 00:01:58 -0400

    Commit message

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -58,4 +58,4 @@ export default function RootLayout(props: {
 // Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
 // Unfortunately, this also means it will also disable the automatic migration of the database
 // And, you will have to manually migrate it with `drizzle-kit push`
-export const runtime = 'edge';
+// export const runtime = 'edge';
